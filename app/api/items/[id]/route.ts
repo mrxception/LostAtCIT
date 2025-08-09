d404d427cfdd14bffb1310f3a7ca0c1518f3350c
mrxception <mrxmrx459@gmail.com>
2025-08-09 22:43:53 +0800

    fixed some errors

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -1,6 +1,8 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { query } from "@/lib/database"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   try {
     const itemId = Number.parseInt(params.id)
